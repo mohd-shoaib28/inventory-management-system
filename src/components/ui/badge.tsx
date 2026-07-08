7b80420a4334552e2cfac017edfbff1aae289bb2
Mohd Shoaib <shoaibmail30@gmail.com>
2026-07-08 13:30:18 +0530

    changed border roundnes

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -18,7 +18,7 @@ function Badge({ className, variant = "default", ...props }: BadgeProps) {
   return (
     <div
       className={cn(
-        "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-medium transition-colors",
+        "inline-flex items-center border px-2.5 py-0.5 text-xs font-medium transition-colors",
         variants[variant],
         className
       )}
